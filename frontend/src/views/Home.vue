d797f590f9ee5978fd90cbc724c45e2401bd6496
OCEAN <1691523060@qq.com>
2021-08-20 20:11:43 +0800

    jhy--singlechoice

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SingleChoose/>
   </div>
 </template>
-
+<script>
+import SingleChoose from "@/components/QuestionTemplates/SingleChoose";
+export default {
+  components: {SingleChoose}
+}
+</script>
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SingleChoose from '@/components/QuestionTemplates/SingleChoose'
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    SingleChoose
   }
 }
 </script>
