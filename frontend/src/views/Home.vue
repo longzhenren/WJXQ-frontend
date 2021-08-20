d9f8b858502a4ed5c348b75fff81820a26b43a8f
OCEAN <1691523060@qq.com>
2021-08-20 20:11:43 +0800

    jhy--singlechoice

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <SingleChoose/>
+  </div>
+</template>
+<script>
+import SingleChoose from "@/components/QuestionTemplates/SingleChoose";
+export default {
+  components: {SingleChoose}
+}
+</script>
+<script>
+// @ is an alias to /src
+import SingleChoose from '@/components/QuestionTemplates/SingleChoose'
+export default {
+  name: 'Home',
+  components: {
+    SingleChoose
+  }
+}
+</script>
