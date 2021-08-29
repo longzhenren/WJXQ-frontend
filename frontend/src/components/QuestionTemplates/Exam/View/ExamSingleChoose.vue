4ceb5523d6438daaec270728d5511298d408f8ae
OCEAN <1691523060@qq.com>
2021-08-29 16:48:23 +0800

    考试单多选更新

--- frontend/src/components/QuestionTemplates/Exam/View/ExamSingleChoose.vue
+++ frontend/src/components/QuestionTemplates/Exam/View/ExamSingleChoose.vue
@@ -1,31 +1,34 @@
 
 
 <template>
-
-  <div  class="InnerDiv" >
+  <div class="InnerDiv">
     <div >
 
       <label v-if="QesData.Must==true" style="color: red" >*</label>
-      <label  >考试单选- {{ QesData.question }}</label>
+      <label  >考试多选- {{ QesData.question }}</label>
       <label  style="color: red;font-size: 14px" > （{{ QesData.score }}分）</label>
     </div>
     <div >
       <label  class="describe"> {{ QesData.describe }} </label>
     </div>
-    <el-radio-group v-model="QesData.radio" class="InnerDiv" style="width: 100%" >
+
+    <el-checkbox-group v-model="QesData.radio" class="InnerDiv">
       <el-row :gutter="10" class="Choice" type="flex" align="top" justify="left"  v-for="(choice,i) in QesData.choices" >
         <el-col :span="24">
-          <el-radio class="Choice" :label="QesData.choices[i]" >
-          </el-radio>
+          <el-checkbox :label="QesData.choices[i]" :key="choice"
+                       class="Choice"
+          >
+          </el-checkbox>
+
         </el-col>
 
+        <!--      <el-col :span="10" v-if="QesData.Rate==true">-->
+        <!--        <el-progress :percentage="0"  ></el-progress>-->
+        <!--      </el-col>-->
 
       </el-row>
-    </el-radio-group>
 
-    <!--    <div class="RightDiv">-->
-    <!--      <el-divider content-position="right"> <el-button  icon="el-icon-edit" type="primary" @click="edit" class="RightElement"  >修改</el-button></el-divider>-->
-    <!--    </div>-->
+    </el-checkbox-group >
   </div>
 </template>
 
@@ -61,8 +64,53 @@ export default {
         score:1,
         edit:1,
         Must:true,
+        HalfRightScore:0.5,
       }
     };
+  },
+  mounted() {
+    // console.log(this.FatherData)
+    if ( this.FatherData.edit!==null && this.FatherData.edit!=={} && this.FatherData.edit!==undefined ) {
+      this.QesData.edit = this.FatherData.edit;
+      this.QesData.question = this.FatherData.question;
+      this.QesData.choices = this.FatherData.choices;
+      this.QesData.radio = this.FatherData.radio;
+      this.QesData.Number = this.FatherData.Number
+      this.QesData.describe = this.FatherData.describe;
+      this.QesData.max = this.FatherData.max;
+      this.QesData.min = this.FatherData.min;
+      this.QesData.Must = this.FatherData.Must;
+      this.QesData.Amount = this.FatherData.Amount;
+    }
+    // this.multiChoice.edit = this.FatherData.edit;
+    // this.multiChoice.question = this.FatherData.question;
+    // this.multiChoice.choices = this.FatherData.choices;
+    // this.multiChoice.radio = this.FatherData.radio;
+  },
+  watch: {
+    FatherData(newData,oldData){
+      if ( newData.edit!==null && newData.edit!=={} && newData.edit!==undefined ){
+        this.QesData = newData;
+        return
+      }
+      else  {
+        this.QesData = {
+          id:"",
+          number:"",
+          describe: "这是一个描述",
+          question:"",
+          choices:["选项1","选项2"],
+          radio: 0,
+
+          //settings
+          max:1,
+          min:1,
+          edit:1,
+          Must:true,
+          Amount:true
+        }
+      }
+    },
   },
   created() {
     bus.$on('SaveData',this.saveData)
@@ -72,65 +120,46 @@ export default {
     bus.$off('SaveData',this.saveData)
     bus.$off('changeData',this.changeData)
   },
-  mounted() {
-    // console.log(this.FatherData)
-    if ( this.FatherData.question!==null && this.FatherData.question!=={} && this.FatherData.question!==undefined ){
-      this.QesData.edit = this.FatherData.edit;
-      this.QesData.question = this.FatherData.question;
-      this.QesData.choices = this.FatherData.choices;
-      this.QesData.describe = this.FatherData.describe;
-      this.QesData.radio = this.FatherData.radio;
-      this.QesData.Must = this.FatherData.Must;
-      // this.singleChoice = this.FatherData
-    }
-  },
-
-  watch: {
-    FatherData(newData,oldData){
-      if ( newData.edit!==null && newData.edit!=={} && newData.edit!==undefined ){
-        this.QesData = newData;
-      }
-      else  {
-        this.QesData = {
-          id:"",
-          number:"",
-          describe: "",
-          question:"",
-          choices:["选项1","选项2"],
-          radio: 0,
-
-          score:1,
-          edit:1,
-          Must:true,
-        }
-      }
-    },
-  },
   methods: {
+    // 保存数据
     // 保存数据
     saveData(QesData,index){
       console.log("要保存的题号："+index)
-      console.log("本体题号:"+this.QesData.ItemIndex)
+      console.log("本题题号:"+this.ItemIndex)
       if (index===this.ItemIndex){
-        console.log(index)
-        console.log(this.QesData)
+
         this.QesData = QesData
         this.save()
       }
     },
 
     changeData(QesData,index){
-      // console.log(index)
-      // console.log(this.QesData.Number)
+      console.log("要同步的题号："+index)
+      console.log("本题题号:"+this.ItemIndex)
       if (index===this.ItemIndex){
+        console.log("进入同步")
         this.QesData = QesData
       }
     },
-
+    del:function (i){
+      if(this.QesData.choices>=2)
+      {
+        this.QesData.choices.splice(i,1)
+        if(this.QesData.max>this.QesData.choices.length){
+          this.QesData.max=this.QesData.choices.length
+        }
+        if(this.QesData.min>this.QesData.choices.length){
+          this.QesData.min=this.QesData.choices.length
+        }
+      }
+      else {
+        this.$message.warning("选项不可以少于2")
+      }
+    },
     add: function() {
       this.QesData.choices.push("")
     },
-    save: function() {
+    save() {
       let find = false;
       for (let i = 0; i < this.QesData.choices.length; i++) {
         for (let j = i + 1; j < this.QesData.choices.length; j++) {
@@ -143,6 +172,7 @@ export default {
       if(find){
         alert("不可以存在重复项")
       }else {
+        console.log("子组件开始保存")
         this.QesData.edit=0
         this.$emit('SaveQes',this.QesData)
       }
